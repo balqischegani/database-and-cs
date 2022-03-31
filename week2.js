d62957a5589c8ec884b124b8ffc677361424a64f
balqischegani <101814404+balqischegani@users.noreply.github.com>
2022-03-31 22:41:25 +0800

    Add files via upload

---
+++ week2.js
@@ -0,0 +1,16 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.qc7eq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  if (err)
+  {
+      console.log(err.message)
+      return
+  }  
+  console.log('Connected to MongoDB');
+  
+   const collection =client.db("test").collection("devices");
+   client.close();
+  
+   
+  });
